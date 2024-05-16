b13496199f41bf7d637aded4bb625f509c37f05f
mariahuamanosores71750533 <167445122+mariahuamanosores71750533@users.noreply.github.com>
2024-05-16 10:48:18 -0500

    updating

---
+++ nosotros.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title> sobre nosotros</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="MyraStudio" name="author" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="plantilla/Admin/vertical/assets/images/favicon.ico">
+
+    <!-- App css -->
+    <link href="plantilla/Admin/vertical/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="plantilla/Admin/vertical/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="plantilla/Admin/vertical/assets/css/theme.min.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+    <?php include('include/menu.php') ?>
+    <br><br>
+
+    <br><br><br><div class="container">
+        <div class="row">
+            <div class="col-md-6 espaciado-imagen">
+                <img src="tienda.jpg" class="img-fluid" alt="Imagen">
+            </div>
+            <div class="col-md-6 espaciado-contenido">
+                <h2>SOBRE NOSOTROS</h2>
+                <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dapibus lacus id eros condimentum, in condimentum metus gravida. Sed efficitur, purus nec fermentum varius, ipsum velit efficitur lorem, et efficitur leo nisi eget velit. Vivamus id turpis ac arcu ullamcorper consectetur vel nec nisi. Fusce consectetur posuere lorem, a varius libero facilisis vel. Proin at sapien ut felis vestibulum rutrum eget vel ligula. Vestibulum nec posuere nulla. Nulla facilisi. Nulla facilisi.</p>
+                <p class="text-justify">Integer in lectus faucibus, aliquet lacus sed, rutrum magna. Vivamus non nisl ultrices, fermentum turpis non, faucibus nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Cras non tempus neque. Proin porttitor, justo vel gravida volutpat, justo turpis tempor mi, id lacinia metus felis nec nibh. Nam eu convallis lectus. Phasellus mattis arcu vel mauris mollis, quis cursus magna bibendum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla facilisi. Sed ultricies, libero eu lobortis facilisis, elit ligula euismod odio, ac venenatis odio elit sed libero. Nullam lobortis est in mauris consectetur, a viverra elit vehicula. Phasellus interdum lorem vitae ante hendrerit, nec fermentum magna tempus. Fusce sed lectus a magna faucibus varius.</p>
+            </div>
+        </div>
+        <?php include('include/pie_pagina.php') ?>
+    </div>
+
+
+
+
+    <!-- jQuery  -->
+    <script src="plantilla/Admin/vertical/assets/js/jquery.min.js"></script>
+    <script src="plantilla/Admin/vertical/assets/js/bootstrap.bundle.min.js"></script>
+    <script src="plantilla/Admin/vertical/assets/js/metismenu.min.js"></script>
+    <script src="plantilla/Admin/vertical/assets/js/waves.js"></script>
+    <script src="plantilla/Admin/vertical/assets/js/simplebar.min.js"></script>
+
+    <!-- App js -->
+    <script src="plantilla/Admin/vertical/assets/js/theme.js"></script>
+
+</body>
+
+</html>
